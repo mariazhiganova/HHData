2e61d75421f0435d9ab40edf0e7a50559097c80c
maria_zhiganova <my.nik.mariann@gmail.com>
2025-05-01 21:27:55 +0300

    add changes

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,6 @@
-import json
+from typing import Dict, List
 
-from src.api_connection import EmployerHH
+from src.api_connection import EmployerHH, Vacancy
 
 
 def select_employers_ids():
@@ -23,42 +23,62 @@ def select_employers_ids():
             for employer_id, employer_name in employer.items():
                 print(f"{idx + 1}. ID: '{employer_id}', Название: '{employer_name}'")
 
-        selected_ids = input("Введите номера работодателей для добавления в список (через запятую): ")
+        selected_ids = input(
+            "Введите номера работодателей для добавления в список (через запятую): "
+        )
 
-        selected_indices = selected_ids.split(',')
+        selected_indices = selected_ids.split(",")
         for index in selected_indices:
             try:
                 index = int(index.strip()) - 1
                 if 0 <= index < len(search_results):
                     employer_id = list(search_results[index].keys())[0]
                     if employer_id not in employers_id:
-                        employers_id.append(employer_id)
-                        print(f"Работодатель с ID {employer_id} добавлен.")
+                        vacancies = Vacancy()._connection(employer_id)
+                        if vacancies:
+                            employers_id.append(employer_id)
+                            print(f"Работодатель с ID {employer_id} добавлен.")
+                        else:
+                            print(
+                                f"Работодатель с ID {employer_id} не имеет вакансий и не будет добавлен."
+                            )
                     else:
                         print(f"Работодатель с ID {employer_id} уже добавлен.")
                 else:
-                    print(f"Неверный номер: {index + 1}. Пожалуйста, выберите номер из списка.")
+                    print(
+                        f"Неверный номер: {index + 1}. Пожалуйста, выберите номер из списка."
+                    )
             except ValueError:
-                print(f"Некорректный ввод: {index}. Пожалуйста, введите номера через запятую.")
+                print(
+                    f"Некорректный ввод: {index}. Пожалуйста, введите номера через запятую."
+                )
 
-        print(f"Текущий список выбранных работодателей: {employers_id}")
+        if len(employers_id) < 10:
+            print(
+                "У вас недостаточно работодателей с вакансиями. Пожалуйста, добавьте больше."
+            )
+            print(f"Текущий список выбранных работодателей: {employers_id}")
+        else:
+            print(
+                "Вы успешно выбрали достаточное количество работодателей с вакансиями."
+            )
 
-    print("Вы достигли максимального количества работодателей.")
     return employers_id
 
 
-def get_full_employers_info(employer_ids):
+def get_full_employers_info(employer_ids: List[int]) -> Dict[int, Dict]:
     """
     Вспомогательная функция для получения информации о работодателях по их ID.
     """
-    employer_info_list = {}
+    employer_info_dict = {}
     employers = EmployerHH()
 
     for employer_id in employer_ids:
-        info_list = employers._connection(employer_id)  # Получаем список работодателей
-        if info_list:  # Если список не пустой
-            employer_info_list[employer_id] = info_list[0]  # Берем первый элемент списка
+        info = employers.get_employer_info(employer_id)
+        if info:
+            employer_info_dict[employer_id] = info
         else:
-            employer_info_list[employer_id] = {}  # Если нет данных, добавляем пустой словарь
+            print(f"Информация о работодателе с ID {employer_id} не найдена.")
+            employer_info_dict[employer_id] = {}
 
-    return employer_info_list
+    return employer_info_dict
