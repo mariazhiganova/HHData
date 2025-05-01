162e9874424990f980b4cad49e83e2f5cdb72c8c
maria_zhiganova <my.nik.mariann@gmail.com>
2025-05-01 21:27:50 +0300

    add changes

--- src/db_worker.py
+++ src/db_worker.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import psycopg2
 
 
@@ -8,91 +10,110 @@ class DBManager:
         """
         try:
             self.connection = psycopg2.connect(
-                host=host,
-                database=database,
-                user=user,
-                password=password
+                host=host, database=database, user=user, password=password
             )
         except psycopg2.Error as e:
             print(f"Ошибка при подключении к БД: {e}")
 
-    def close_connection(self):
+    def close_connection(self) -> None:
         """Закрытие соединения с БД."""
         if self.connection:
             self.connection.close()
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> None | list[Any]:
         """
         Получает список всех компаний и количество вакансий у каждой компании
         """
         try:
             with self.connection.cursor() as cursor:
-                cursor.execute('''SELECT e.name, COUNT(v.id) AS vacancy_count FROM employers e
-                LEFT JOIN vacancies v USING(employer_id)
-                GROUP BY e.name;
-                ''')
-                return cursor.fetchall()
+                cursor.execute(
+                    """SELECT e.name, COUNT(v.id) AS vacancy_count 
+                    FROM employers e
+                    LEFT JOIN vacancies v USING(employer_id)
+                    GROUP BY e.name;
+                    """
+                )
+                data = cursor.fetchall()
+                if data:
+                    return data
 
         except psycopg2.Error as e:
             print(f"Ошибка при получении данных: {e}")
             return []
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> None | list:
         """
         Получает список всех вакансий с указанием названия компании,
-        названия вакансии и зарплаты и ссылки на вакансию.
+        названия вакансии, зарплаты и ссылки на вакансию.
         """
         try:
             with self.connection.cursor() as cursor:
-                cursor.execute('''SELECT v.name AS vacancy_name, e.name AS company_name, v.salary, v.url 
-                FROM vacancies v
-                LEFT JOIN employers e USING(employer_id);
-                ''')
-                return cursor.fetchall()
+                cursor.execute(
+                    """SELECT v.name AS vacancy_name, e.name AS company_name, v.salary, v.url 
+                    FROM vacancies v
+                    LEFT JOIN employers e USING(employer_id);
+                    """
+                )
+                data = cursor.fetchall()
+                if data:
+                    return data
 
         except psycopg2.Error as e:
             print(f"Ошибка при получении данных: {e}")
             return []
 
-    def get_avg_salary(self):
+    def get_avg_salary(self) -> None | list[Any]:
         """
-         Получает среднюю зарплату по вакансиям.
+        Получает среднюю зарплату по вакансиям.
         """
         try:
             with self.connection.cursor() as cur:
-                cur.execute('''
-                  SELECT AVG(salary) FROM vacancies;''')
-                return cur.fetchall()
+                cur.execute(
+                    """
+                  SELECT ROUND(AVG(salary)) FROM vacancies;"""
+                )
+                data = cur.fetchall()
+                if data:
+                    return data
 
         except psycopg2.Error as e:
             print(f"Ошибка при получении данных: {e}")
             return []
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self) -> None | list:
         """
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         """
         try:
             with self.connection.cursor() as cur:
-                cur.execute('''SELECT * FROM vacancies
+                cur.execute(
+                    """SELECT * FROM vacancies
                 WHERE salary > (SELECT AVG(salary) FROM vacancies)
-                ''')
-                return cur.fetchall()
+                """
+                )
+                data = cur.fetchall()
+                if data:
+                    return data
 
         except psycopg2.Error as e:
             print(f"Ошибка при получении данных: {e}")
             return []
 
-    def get_vacancies_with_keyword(self, keyword):
+    def get_vacancies_with_keyword(self, keyword) -> None | list:
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
         """
         try:
             with self.connection.cursor() as cur:
-                cur.execute(f'''SELECT * FROM vacancy
+                cur.execute(
+                    """SELECT * FROM vacancies
                 WHERE name LIKE %s;
-                ''', (f'%{keyword}%',))
-                return cur.fetchall()
+                """,
+                    (f"%{keyword}%",),
+                )
+                data = cur.fetchall()
+                if data:
+                    return data
 
         except psycopg2.Error as e:
             print(f"Ошибка при получении данных: {e}")
