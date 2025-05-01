82e91777bd5a0cefd0b9625c2943606d217b13e4
maria_zhiganova <my.nik.mariann@gmail.com>
2025-05-01 21:27:21 +0300

    add changes

--- src/api_connection.py
+++ src/api_connection.py
@@ -1,8 +1,13 @@
 from abc import ABC, abstractmethod
+from typing import Any, Optional, Dict
+
 import requests
 
 
 class BaseParser(ABC):
+    """
+    Базовый класс для создания классов для подключения к АПИ с методом подключения
+    """
     def __init__(self):
         self.base_url = "https://api.hh.ru/"
 
@@ -16,11 +21,11 @@ class EmployerHH(BaseParser):
     Класс для работы с API HeadHunter.
     """
 
-    def _connection(self, keyword) -> list:
+    def _connection(self, keyword: str) -> list:
         """
         Метод, который подключается к API hh.ru и получает работодателей по ключевому слову.
         """
-        url = f"{self.base_url}employers?text={keyword}"
+        url = f"{self.base_url}employers?text={keyword}&page=0&per_page=100"
 
         response = requests.get(url)
 
@@ -30,17 +35,32 @@ class EmployerHH(BaseParser):
             print(f"Запрос не был успешным. Возможная причина: {response.reason}")
             return []
 
-    def get_employer_ids(self, keyword) -> list:
+    def get_employer_ids(self, keyword: str) -> list[dict[Any, Any]] | None:
         employers = self._connection(keyword)
 
         ids = []
         for employer in employers:
-            data = {employer['id']: employer['name']}
+            data = {employer["id"]: employer["name"]}
             ids.append(data)
         if ids:
             return ids
         else:
-            print('Айди не найдены')
+            print("Айди не найдены")
+
+    def get_employer_info(self, employer_id: int) -> Optional[Dict]:
+        """
+        Метод для получения информации о работодателе по его ID.
+        """
+        url = f"{self.base_url}employers/{employer_id}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(
+                f"Не удалось получить информацию о работодателе {employer_id}. Причина: {response.reason}"
+            )
+            return None
 
 
 class Vacancy(BaseParser):
@@ -48,7 +68,7 @@ class Vacancy(BaseParser):
     Класс для работы с API HeadHunter.
     """
 
-    def _connection(self, employer_id):
+    def _connection(self, employer_id: int) -> None | list:
         """
         Метод, который подключается к апи hh.ru и получает вакансии по айди работодателя в формате json словарей
         """
@@ -62,14 +82,3 @@ class Vacancy(BaseParser):
         else:
             print(f"Запрос не был успешным. Возможная причина: {response.reason}")
             return []
-
-
-if __name__ == '__main__':
-    data = EmployerHH()
-    for e in data.get_employer_ids('Т-банк'):
-        print(e)
-    for e in data._connection('78638'):
-        print(e)
-
-    vacancies = Vacancy()
-    print(vacancies._connection('78638'))
